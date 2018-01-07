72e666d323109dc3060b1bd0b8344debcaf6901d
Arvis Abromovics <info@vsmedio.lv>
2018-01-07 12:47:40 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,36 @@
+/*
+Программист создает человека
+Создать class Person. У человека должно быть имя String name, возраст int age.
+Добавь метод initialize(String name, int age), в котором проинициализируй переменные name и age.
+В методе main создай объект Person, занеси его ссылку в переменную person.
+Вызови метод initialize с любыми значениями.
+
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. У класса Person должна быть переменная name с типом String.
+3. У класса Person должна быть переменная age с типом int.
+4. У класса Person должен быть метод initialize, принимающий в качестве параметра имя,
+возраст и инициализирующий соответствующие переменные класса.
+5. Необходимо создать объект типа Person.
+6. Необходимо вызвать метод initialize у созданного объекта и передать в него какие-либо параметры.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Person person = new Person ();
+        person.initialize("Alex", 40);
+    }
+
+    static class Person {
+        //напишите тут ваш код
+        private String name;
+        private int age;
+
+        public void initialize(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
